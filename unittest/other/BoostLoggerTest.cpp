84ff67c2195a8cb06529cf8418cb20086c9bd704
yunccll <yunccll@sina.com>
2019-07-08 17:36:17 +0800

    add the boost log test code

--- unittest/other/BoostLoggerTest.cpp
+++ unittest/other/BoostLoggerTest.cpp
@@ -18,7 +18,12 @@
 #include <ostream>
 #include <boost/log/core.hpp>
 
+#include <boost/log/utility/setup/console.hpp>  //add_console_log
+
+
+#include <boost/log/sources/severity_feature.hpp>
 #include <boost/log/sources/severity_logger.hpp>        // severity_logger
+#include <boost/log/utility/manipulators/to_log.hpp>
 
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/sources/global_logger_storage.hpp> //BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT
@@ -33,12 +38,15 @@
 #include <boost/log/expressions.hpp>
 
 #include <boost/log/attributes.hpp>
+#include <boost/log/support/date_time.hpp>
+
 
 namespace logging = boost::log;
 namespace sinks = boost::log::sinks;
 namespace keywords = boost::log::keywords;
 namespace src = boost::log::sources;
 namespace attrs = boost::log::attributes;
+namespace expr = boost::log::expressions;
 
 
 
@@ -50,7 +58,7 @@ void init(){
         keywords::file_name = "main_%N.log",
         keywords::rotation_size = 10 * 1024 * 1024,
         keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
-        keywords::format = "[%TimeStamp%] [%LineID%] %Message%"
+        keywords::format = "[%TimeStamp%] [%LineID%] [%Severity%] %Message%"
     );
     //set level
     logging::core::get()->set_filter
@@ -89,11 +97,11 @@ TEST(BoostLog, globalLogger){
     src::logger_mt & lg = my_logger::get();
     BOOST_LOG(lg) << "Hello World";
 }
-
 */
 
 
-enum severity_level {
+
+enum severity_level11 {
     normal,
     notification,
     warning,
@@ -101,17 +109,15 @@ enum severity_level {
     critical,
 };
 
-
 template<typename CharT, typename Traits> 
 inline std::basic_ostream<CharT, Traits> & operator<<(
-    std::basic_ostream<CharT, Traits> & os, severity_level lvl){
-     static const char* const str[] =
-     {
-         "normal",
-         "notification",
-         "warning",
-         "error",
-         "critical"
+    std::basic_ostream<CharT, Traits> & os, severity_level11 lvl){
+     static const char* const str[] = {
+         "NORMAL",
+         "NOTIFICATION",
+         "WARNIng",
+         "ERROR",
+         "CRITIcal"
      };
      if (static_cast< std::size_t >(lvl) < (sizeof(str) / sizeof(*str)))
          os << str[lvl];
@@ -120,31 +126,116 @@ inline std::basic_ostream<CharT, Traits> & operator<<(
      return os;
 }
 
-/*  
+//logging::formatting_ostream& operator<<
+//(
+//    logging::formatting_ostream& strm,
+//    logging::to_log_manip< severity_level11, tag::severity > const& manip
+//)
+//{
+//    static const char* strings[] =
+//    {
+//        "NORM",
+//        "NTFY",
+//        "WARN",
+//        "ERRR",
+//        "CRIT"
+//    };
+//
+//    severity_level11 level = manip.get();
+//    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
+//        strm << strings[level];
+//    else
+//        strm << static_cast< int >(level);
+//
+//    return strm;
+//}
+
+
+
+// * for log 's format 
 BOOST_LOG_ATTRIBUTE_KEYWORD(line_id, "LineID", unsigned int)
-BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity_level)
-BOOST_LOG_ATTRIBUTE_KEYWORD(tag_attr, "Tag", std::string)
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+
 BOOST_LOG_ATTRIBUTE_KEYWORD(scope, "Scope", attrs::named_scope::value_type)
 BOOST_LOG_ATTRIBUTE_KEYWORD(timeline, "Timeline", attrs::timer::value_type)
-*/
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(severity, "Severity", severity_level11) 
+
+
+
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(tag_attr, "Tag", std::string)
 
 TEST(BoostLog, severityLevelLogger){
+
+    logging::add_console_log(std::clog, keywords::format = "[%TimeStamp%]: %_%");
+
     logging::add_file_log(
         keywords::file_name = "main_%N.log",
         keywords::rotation_size = 10 * 1024 * 1024,
         keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
-        keywords::format = "[%TimeStamp%] [%LineID%] [%TimeLine%] [%Tag%] %Message%"
+//        keywords::format = "[%TimeStamp%] [%LineID%] [%Severity%] %Message%"
+
+        keywords::format = expr::stream
+            << expr::format_date_time(timestamp, "%Y-%m-%d, %H:%M:%S.%f")
+            << " [" << expr::format_date_time(timeline, "%O:%M:%S")
+            << "] [" << expr::format_named_scope(scope, keywords::format = "%n (%f:%l)")
+            << "] [" << tag_attr 
+            << "] <" << severity
+            << "> " << expr::message
+
     );
 
-    ///BOOST_LOG_NAMED_SCOPE("named_scope_logging");
-    src::severity_logger<severity_level> slog;
+    src::logger lg;
+    BOOST_LOG(lg) <<  "only message" ; //only  message
 
-    slog.add_attribute("Tag", attrs::constant<std::string>("My tag value"));
-    slog.add_attribute("LineID",  attrs::counter< unsigned int >(1));
-    slog.add_attribute("TimeStamp",  attrs::local_clock());
-    slog.add_attribute("TimeLine",  attrs::timer());
+    logging::add_common_attributes(); // for timestamp, lineId, process_id, thread_id
+
+    BOOST_LOG(lg) <<  "only common message" ; // add common message
+
+    logging::core::get()->add_thread_attribute("Scope", attrs::named_scope()); //for scope attr
+    BOOST_LOG_FUNCTION();   // for scope 's function & line id 
+
+    BOOST_LOG(lg) <<  "function & file & line no" ; // add function & file & line no
 
 
-    BOOST_LOG_SEV(slog, normal) << "Normal Hello world";
-    BOOST_LOG_SEV(slog, notification) << "Notification Hello world";
+    src::severity_logger< severity_level11 > slg;
+    slg.add_attribute("Timeline", attrs::timer()); //add my timeline attribute for the log source
+    slg.add_attribute("Tag", attrs::constant<std::string>("chenglun")); // add my tag attribute for the log source
+
+    BOOST_LOG_SEV(slg, normal) << "severity log: A regular message";
+    BOOST_LOG_SEV(slg, warning) << "severity log: Something bad is going on but I can handle it";
+    BOOST_LOG_SEV(slg, critical) << "severity log: Everything crumbles, shoot me now!";
 }
+
+
+
+
+//TEST(BoostLog, severityLevelLogger){
+//
+//    logging::add_file_log(
+//        keywords::file_name = "main.log",
+//        keywords::rotation_size = 10 * 1024 * 1024,
+//        keywords::time_based_rotation = sinks::file::rotation_at_time_point(0, 0, 0),
+//        keywords::open_mode = std::ios::out | std::ios::app,
+//        keywords::format = "[%TimeStamp%] [%LineID%] [%TimeLine%] [%Tag%] [%Severity%] %Message%"
+//    );
+//
+//    ///BOOST_LOG_NAMED_SCOPE("named_scope_logging");
+//    src::severity_logger<severity_level11> slog;
+//
+//
+//    logging::core::get()->set_filter(expr::attr< severity_level11>("Severity") >= notification);
+//
+//    slog.add_attribute("LineID",  attrs::counter< unsigned int >(1));
+//    slog.add_attribute("TimeStamp",  attrs::local_clock());
+//    slog.add_attribute("TimeLine",  attrs::timer());
+//    slog.add_attribute("Tag", attrs::constant<std::string>("My tag value"));
+//
+//
+//    BOOST_LOG_SEV(slog, normal) << "normal Hello world";
+//    BOOST_LOG_SEV(slog, notification) << "notification Hello world";
+//    BOOST_LOG_SEV(slog, warning) << "warning Hello world";
+//    BOOST_LOG_SEV(slog, error) << "error Hello world";
+//    BOOST_LOG_SEV(slog, critical) << "critical Hello world";
+//}
